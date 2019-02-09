aa51aecbb09d3e313b89e7bdf9e8fbd5e27a474c
Raspberry Pi <nrober@umich.edu>
2019-02-09 14:28:17 -0500

    updateFML

--- distance_sensor.py
+++ distance_sensor.py
@@ -18,20 +18,20 @@ try:
 
       print "Calculating distance"
 
-      while 1=1:
+      while 1==1:
           GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-          time.sleep(0.00001)
+          time.sleep(0.0001)
 
           GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
           while GPIO.input(PIN_ECHO)==0:
-          pulse_start_time = time.time()
+	         pulse_start_time = time.time()
           while GPIO.input(PIN_ECHO)==1:
-          pulse_end_time = time.time()
+        	 pulse_end_time = time.time()
 
-          pulse_duration = pulse_end_time - pulse_start_time
-          distance = round(pulse_duration * 17150, 2)
+         	 pulse_duration = pulse_end_time - pulse_start_time
+         	 distance = round(pulse_duration * 17150, 2)
           print "Distance:",distance,"cm"
 
 finally:
