226802204ea2052d7c366c1b3a805703a9b79411
Raspberry Pi <nrober@umich.edu>
2019-02-09 17:17:24 -0500

    edited amtuko's fucked up arduino script

--- ArduinoPIInterface.py
+++ ArduinoPIInterface.py
@@ -1,5 +1,5 @@
 import serial
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 
 ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
@@ -7,17 +7,17 @@ ser.baudrate=9600
 def blink(pin):
 
 
-GPIO.output(pin,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(pin,GPIO.LOW)
-time.sleep(1)
-return
+#GPIO.output(pin,GPIO.HIGH)
+	time.sleep(1)
+#GPIO.output(pin,GPIO.LOW)
+	time.sleep(1)
+	return
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(11, GPIO.OUT)
 while True:
 
-read_ser=ser.readline()
-print(read_ser)
-if(read_ser=="Hello From Arduino!"):
-blink(11)
+	read_ser=ser.readline()
+	print(read_ser)
+	if(read_ser=="Hello From Arduino!"):
+		print("ur ugly")
