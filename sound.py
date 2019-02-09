98a9ed512b1e8dbab972584b9d3f8f40ab666409
Rosie Van Alsburg <33611942+roseva@users.noreply.github.com>
2019-02-09 15:16:50 -0500

    hopefully updated to correct syntax for sound sensor

--- sound.py
+++ sound.py
@@ -5,12 +5,13 @@ class sound_sensor:
   'Class that initializes and uses the big noise sensor for the THESEUS robot'
   
   def __init__(self):
-    # init sensor
+    # init sound sensor
+    pin_num = 12
+    GPIO.setmode(GPIO.BOARD) # use board pin numbers
+    GPIO.setup(pin_num, GPIO.IN)
+    
     
   def ambulance_approaching(self):
     # check to see if there is a loud ambulance noise, if so, return true
-    
-    
-
-
-
+    if GPIO.input(pin_num)==1:
+      return True
