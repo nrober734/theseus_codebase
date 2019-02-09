a4cfb15eaeeadbeb0e6f7715ff7fb6741ae40097
Rosie Van Alsburg <33611942+roseva@users.noreply.github.com>
2019-02-09 11:29:10 -0500

    initial commit of rgb architecture

---
+++ rgb.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+import time
+
+# initialize color sensors
+
+# if input is a light color, line is being sensed
+
+# if input is a dark color, adjust back on course until input is a light color again
+
