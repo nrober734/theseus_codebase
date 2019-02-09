d0fb59cc3ee919dfaf0dfa2beb92c12df73434c3
Raspberry Pi <nrober@umich.edu>
2019-02-09 16:39:27 -0500

    camera shiiit

--- sound.py
+++ sound.py
@@ -3,23 +3,29 @@ import time
 
 class sound_sensor:
   'Class that initializes and uses the big noise sensor for the THESEUS robot'
-  
-  def __init__(self):
-    # init sound sensor
-    pin_num = 12
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin_num, GPIO.IN)
-    
-    
-  def ambulance_approaching(self):
-    # check to see if there is a loud ambulance noise, if so, return true
-    clock = 0
-    total = 0
-    while clock != 100:
-      clock += GPIO.input(pin_num)
-      total += 1
-      
-    if clock / total > 0.75:
-      return true
-    else:
-      return false
+	pin_num = 14
+	def __init__(self):
+		# init sound sensor
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(pin_num, GPIO.IN)
+
+	  def ambulance_approaching(self):
+		# check to see if there is a loud ambulance noise, if so, return true
+		clock = 0
+		total = 0
+		while clock != 100:
+			clock += GPIO.input(pin_num)
+			total += 1
+		if clock / total > 0.5:
+			return True
+		else:
+			return False
+
+
+def main():
+	soundy = sound_sensor()
+	while(True):
+		print soundy.ambulance_approaching()
+
+if __name__ == "__main__":
+	main()
