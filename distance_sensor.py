e6c2f674ec1aa66ed6f5abe5c4328d7ed89a7ab9
Alexander P Sena <asena@umich.edu>
2019-02-09 13:30:24 -0500

    Looped sensor reading

--- distance_sensor.py
+++ distance_sensor.py
@@ -18,20 +18,21 @@ try:
 
       print "Calculating distance"
 
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+      while 1=1:
+          GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-      time.sleep(0.00001)
+          time.sleep(0.00001)
 
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+          GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
+          while GPIO.input(PIN_ECHO)==0:
+          pulse_start_time = time.time()
+          while GPIO.input(PIN_ECHO)==1:
+          pulse_end_time = time.time()
 
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print "Distance:",distance,"cm"
+          pulse_duration = pulse_end_time - pulse_start_time
+          distance = round(pulse_duration * 17150, 2)
+          print "Distance:",distance,"cm"
 
 finally:
       GPIO.cleanup()
