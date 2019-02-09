5f116785d9730e991a074bed26c84169a45bf3cd
Rosie Van Alsburg <33611942+roseva@users.noreply.github.com>
2019-02-09 12:14:27 -0500

    added libraries

--- rgb.py
+++ rgb.py
@@ -1,5 +1,14 @@
 import RPi.GPIO as GPIO
 import time
+import board
+import busio
+
+import adafruit_tcs34725
+
+
+# Initialize I2C bus and sensor.
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_tcs34725.TCS34725(i2c)
 
 # initialize color sensors
 
