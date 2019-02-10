750eb99c77c206353f4957fc69c898725119ee9b
amtuko <amtuko@Amits-MacBook-Pro.local>
2019-02-09 23:17:17 -0500

    plz again

--- rgb.py
+++ rgb.py
@@ -49,12 +49,14 @@ class rgb_sensor:
     def ardu_RGB_input(self,ser_ch):
         rgblist = []
         counter = 0
-        while counter < 9:
-            rgblist.append(self.raw_ardu_input(ser_ch))
+        while count < 5:
+            if self.raw_ardu_input[0][2] is not ' '
+                rgblist.append(self.raw_ardu_input(ser_ch)[0][2])
+
             counter+=1
 
 
-        return rgblist[0][0]
+        return rgblist
 
     def getR(self):
         return self.r
