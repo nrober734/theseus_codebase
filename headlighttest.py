4150a65a1964bbc7037341007b05f096aa749135
amtuko <amtuko@Amits-MacBook-Pro.local>
2019-02-10 03:19:52 -0500

    v2

--- headlighttest.py
+++ headlighttest.py
@@ -4,7 +4,9 @@ import RPi.GPIO as GPIO
 right_headlamp = 24
 photo_sensor = 19
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(right_headlamp, GPIO.OUT)
 GPIO.setup(photo_sensor,GPIO.IN)
 
-print(GPIO.input(photo_sensor))
+while True:
+    print(GPIO.input(photo_sensor))
