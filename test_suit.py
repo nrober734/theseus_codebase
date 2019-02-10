6af4ac8796e12e26d128462723415f1851d09483
amtuko <amtuko@Amits-MacBook-Pro.local>
2019-02-09 22:42:43 -0500

    fasd

--- test_suit.py
+++ test_suit.py
@@ -9,13 +9,13 @@ ser.baudrate = 9600
 
 rbginsberg = rgb_sensor()
 print ("We aboutta go in")
-#while True:
+while True:
     #print("We made it in bb")
 #results = rbginsberg.ardu_RGB_input(rbginsberg.raw_ardu_input(ser))
-results = rbginsberg.raw_ardu_input(ser)
-print(results)
-results = rbginsberg.raw_ardu_input(ser)
-print(results)
+    results = rbginsberg.raw_ardu_input(ser)
+    print(results)
+# results = rbginsberg.raw_ardu_input(ser)
+# print(results)
 #print("We didn't make it in")
     # for i in results:
     #     print(i)
