a4fb103926a9e9217399a834d523a8a079615bb5
amtuko <amtuko@Amits-MacBook-Pro.local>
2019-02-09 19:41:31 -0500

    hopefully fixed the spaces

--- test_suit.py
+++ test_suit.py
@@ -3,12 +3,12 @@ from config import *
 import serial
 
 def main():
-	ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
-	ser.baudrate = 9600
+    ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+    ser.baudrate = 9600
 
-	ruthbaderginsberg = rgb_sensor()
-	while True:
-    		results = ruthbaderginsberg.ardu_input(ser)
-        	print(results)
+    ruthbaderginsberg = rgb_sensor()
+    while True:
+        results = ruthbaderginsberg.ardu_input(ser)
+        print(results)
     # for i in results:
     #     print(i)
