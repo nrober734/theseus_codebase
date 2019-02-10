2ee1800d5eefd2629628726a84c5f50934152610
amtuko <amtuko@Amits-MacBook-Pro.local>
2019-02-09 21:39:35 -0500

    contstructor error

--- rgb.py
+++ rgb.py
@@ -10,7 +10,7 @@ import adafruit_tcs34725
 class rgb_sensor:
     'Class to initialize and use the rgb sensors for the THESEUS robot'
     def __init__(self):
-        #self.r, self.b, self.g, self.c
+        self.r, self.b, self.g, self.c
         # Initialize I2C bus and sensor.
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.sensor = adafruit_tcs34725.TCS34725(self.i2c)
