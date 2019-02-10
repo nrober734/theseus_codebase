676266058dc356d0aba93a7f9ccf9ceeaafd0c22
amtuko <amtuko@Amits-MacBook-Pro.local>
2019-02-09 19:47:20 -0500

    fixed a c++ error lol

--- test_suit.py
+++ test_suit.py
@@ -2,16 +2,17 @@ from rgb import *
 from config import *
 import serial
 
-def main():
-    ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
-    ser.baudrate = 9600
 
-    ruthbaderginsberg = rgb_sensor()
-    print ("We aboutta go in")
-    while True:
-        print("We made it in bb")
-        results = ruthbaderginsberg.ardu_input(ser)
-        print(results)
-    print("We didn't make it in")
+
+ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser.baudrate = 9600
+
+ruthbaderginsberg = rgb_sensor()
+print ("We aboutta go in")
+while True:
+    print("We made it in bb")
+    results = ruthbaderginsberg.ardu_input(ser)
+    print(results)
+print("We didn't make it in")
     # for i in results:
     #     print(i)
