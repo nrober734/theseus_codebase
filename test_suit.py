138ebaf2ff252e1b2dddd8f533b078b7b2e84a21
amtuko <amtuko@Amits-MacBook-Pro.local>
2019-02-09 19:45:49 -0500

    debugggggggg

--- test_suit.py
+++ test_suit.py
@@ -7,8 +7,11 @@ def main():
     ser.baudrate = 9600
 
     ruthbaderginsberg = rgb_sensor()
+    print ("We aboutta go in")
     while True:
+        print("We made it in bb")
         results = ruthbaderginsberg.ardu_input(ser)
         print(results)
+    print("We didn't make it in")
     # for i in results:
     #     print(i)
